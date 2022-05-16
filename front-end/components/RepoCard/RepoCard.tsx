1002ff7bc1835c99ddd414dfffabcb1bd6503944
jonyejin <i3pivigui@icloud.com>
2022-05-16 15:52:24 +0900

    reorganized directory structure

---
+++ front-end/components/RepoCard/RepoCard.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from '@material-ui/core'
+// import {
+//   getWeatherIconSrc,
+//   fetchOpenWeatherData,
+//   OpenWeatherData,
+//   OpenWeatherTempScale,
+// } from '../../utils/api'
+import './WeatherCard.css'
+
