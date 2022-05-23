eaa6bcb3ae86c5fa0094c8ba8c42b79521927a81
cow-coding <kbp0237@gmail.com>
2022-05-23 16:28:08 +0000

    back-end: redis producer-consumer pipeline code upload

---
+++ data-pipe-line/consumer.py
@@ -0,0 +1,69 @@
+import os
+import json
+import time
+
+import argparse
+
+import yaml
+import schedule
+
+from redisqueue import RedisQueue
+
+from pymongo import MongoClient
+
+
+def db_connect(user, passwd, database, **kwargs):
+    db_connection_str = f"mongodb+srv://{user}:{passwd}@cluster0.dojne.mongodb.net/{database}"
+
+    conn = MongoClient(db_connection_str)
+
+    return conn["final_project"]
+
+
+def insert_data(conn):
+    if len(batch_list) > 0:
+        conn.insert_many(batch_list)
+        print("insert the batch data")
+        batch_list.clear()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--time_interval", type=int, default=9)
+
+    args = parser.parse_args()
+
+    with open("awesome_link.yaml") as f:
+        settings = yaml.load(f, Loader=yaml.FullLoader)
+        
+    cloud_info = settings["cloud_info"]
+    q = RedisQueue("final_project", host=cloud_info["host"], port=6379, db=0, decode_responses=True)
+    db_profile = settings["db_profile"]
+
+    conn = db_connect(**db_profile)
+    conn = conn["repository"]
+
+    global batch_list
+    batch_list = []
+
+    schedule.every(args.time_interval).minutes.do(insert_data, conn)
+    
+    print("Consumer Start!")
+
+    while True:
+        schedule.run_pending()
+        msg = q.get(isBlocking=False)
+
+        if msg is not None:
+            msg = json.loads(msg)
+
+            print(msg)
+            print()
+
+            batch_list.append(msg)
+            time.sleep(3)
+
+
+
+
