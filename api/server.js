c23b9a48ee8ccc3b2bbef92f9dcb98b2e842acf2
hawe66 <youngha908@gmail.com>
2022-05-24 03:00:29 +0000

    added https setting

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -9,11 +10,16 @@ const session = require('express-session');
 const mysql      = require('mysql');
 const dbconfig   = require('./authenticate.json');
 const conn = mysql.createConnection(dbconfig);
-var app = express();
+const app = express();
 
-var server = app.listen(3000, function(){
+const options = { key: fs.readFileSync('/opt/ml/rootca.key'), cert: fs.readFileSync('/opt/ml/rootca.crt') };
+
+// Create an HTTP server. 
+http.createServer(app).listen(3000, function(){
     console.log("Express server has started on port http://127.0.0.1:3000")
 });
+// Create an HTTPS server. 
+https.createServer(options, app).listen(3001);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
