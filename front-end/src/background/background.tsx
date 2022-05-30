a1e12dc124a1c4d6243b5629c908ff7455fee713
jonyejin <i3pivigui@icloud.com>
2022-05-30 11:21:23 +0900

    star click, unclick action detected

--- front-end/src/background/background.tsx
+++ front-end/src/background/background.tsx
@@ -1,34 +1,72 @@
-import { Repo, RepoList, initUser, coldstart } from '../utils/api'
+import { Repo, RepoList, initUser, coldstart, clickedRepo } from '../utils/api'
 
 let isLogin = false
 let username=""
 let starcount=0
 
-// install시 호출된다.
-// chrome.runtime.onInstalled.addListener(() => {
-chrome.cookies.get({
-  url: "https://github.com",
-  name: "logged_in",
-}, function(x){
-  console.log("로그인 여부 확인하기")
-  console.log(x.value)
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse)=>{
+  // TODO
+})
 
-  if (x.value == "yes"){
-    isLogin = true
-  } else {
-    isLogin = false
-  }
 
-  // API 호출해주기
-  if (isLogin){
+/*
+호출 타이밍
+
+- 익스텐션 설치 or 업데이트
+- 백그라운드 페이지가 이벤트를 기다리는 중이었는데 이벤트 생김
+- content script나 다른 extension이 메세지 보냄(Message Passing: `chrome.runtime.sendMessage`)
+- 팝업같은 익스텐션의 다른 뷰가 runtime.getBackgroundPage한다.
+*/
+chrome.storage.sync.get(["isLogin, username"], (res)=> {
+  isLogin = res.isLogin ?? false
+  username = res.username ?? ""
+  
+  if (!isLogin) {
     chrome.cookies.get({
       url: "https://github.com",
-      name: "dotcom_user",
+      name: "logged_in",
     }, function(x){
-      username = x.value
-      // TODO: starcount 못받겠음
-      initUser(username, starcount)
+      console.log("로그인 여부 확인하기")
+      console.log(x.value)
+
+      if (x.value == "yes"){
+        isLogin = true
+      } else {
+        isLogin = false
+      }
+
+      // API 호출해주기
+      if (isLogin){
+        chrome.cookies.get({
+          url: "https://github.com",
+          name: "dotcom_user",
+        }, function(x){
+          username = x.value
+          // 저장하기
+          chrome.storage.sync.set({"isLogin": true, "username": username})
+          
+          // TODO: starcount 못받겠음
+          initUser(username, starcount)
+          
+          // message passing: contentScript를 통해서 
+        })
+      }
     })
   }
 })
-  
+
+function validateGithubView(str){
+  const re: RegExp = /https:\/\/github.com\/[\w\W]+\/[\w\W]+/
+  return re.test(str)
+}
+
+
+chrome.webNavigation.onBeforeNavigate.addListener((details)=>{
+  console.log("-========")
+  // github view
+  if (validateGithubView(details.url)){
+    // 깃헙 레포를 구경했다.
+    console.log("깃헙 레포 구경함~")
+    clickedRepo(username, "")
+  }
+})
--- front-end/src/contentScript/contentScript.tsx
+++ front-end/src/contentScript/contentScript.tsx
@@ -3,12 +3,40 @@ import {inference} from '../utils/api'
 import ReactDOM from 'react-dom'
 import RepoCard from '../popup/RepoCard'
 
-var myBody=document.createElement("h1");
-myBody.innerText="마음에 들조의 추천"
+
+let text = "aaaa"
+// TODO: 
+chrome.runtime.sendMessage("pkopcldfjjedembijjkdkpfifnfmkljf", text)
 
 // 넣을 곳
-var find = document.querySelector(".d-block.Link--secondary.no-underline.f6.mb-3")
-find.insertAdjacentElement('afterend', myBody)
+var find = document.querySelector(".d-block.Link--secondary.no-underline.f6.mb-3") // 클래스들
+if (find != null){
+  var myBody=document.createElement("h1");
+  myBody.innerText="마음에 들조의 추천"
+  find.insertAdjacentElement('afterend', myBody)
+}
+
+var unstar = document.querySelector(".rounded-left-2.border-right-0.btn-sm.btn.BtnGroup-item")
+unstar.addEventListener('click', function(){
+  console.log("스타 해제!")
+})
+var details = document.querySelector(".details-reset.details-overlay.BtnGroup-parent.js-user-list-menu.d-inline-block.position-relative")
+details.addEventListener('click', function(){
+  console.log("디테일 클릭!")
+})
+
+// 주의! star랑 unstar랑 코드 다름.
+var star = document.querySelector(".js-toggler-target.rounded-left-2.btn-sm.btn.BtnGroup-item")
+star.addEventListener('click', function(){
+  console.log("스타 클릭!")
+})  // 이거 코드는 정상 동작 하고 있음.
+
+var details = document.querySelector(".details-reset.details-overlay.BtnGroup-parent.js-user-list-menu.d-inline-block.position-relative")
+details.addEventListener('click', function(){
+  console.log("디테일 클릭!")
+}) // 얘 역시 코드는 정상 동작 하고 있음.
+
+
 
 const App: React.FC<{}> = () => {
   return (
