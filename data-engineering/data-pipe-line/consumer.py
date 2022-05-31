16c68348a7ca38e4612978131a8e4242c9a02bda
cow-coding <kbp0237@gmail.com>
2022-05-31 14:50:09 +0000

    back-end: producer user code modify, consumer code extend version, updater requirements.txt

--- data-engineering/data-pipe-line/consumer.py
+++ data-engineering/data-pipe-line/consumer.py
@@ -1,6 +1,7 @@
 import os
 import json
 import time
+from datetime import datetime
 
 import argparse
 
@@ -30,25 +31,27 @@ def insert_data(conn):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--time_interval", type=int, default=9)
+    parser.add_argument("--time_interval", type=int, default=3)
+    parser.add_argument("--collection", type=str, default="test")
 
     args = parser.parse_args()
 
     with open("awesome_link.yaml") as f:
         settings = yaml.load(f, Loader=yaml.FullLoader)
-        
+
     cloud_info = settings["cloud_info"]
     q = RedisQueue("final_project", host=cloud_info["host"], port=6379, db=0, decode_responses=True)
     db_profile = settings["db_profile"]
 
     conn = db_connect(**db_profile)
-    conn = conn["repository"]
+    conn_repo = conn["repository"]
+    conn = conn[args.collection]
 
     global batch_list
     batch_list = []
 
     schedule.every(args.time_interval).minutes.do(insert_data, conn)
-    
+
     print("Consumer Start!")
 
     while True:
@@ -57,13 +60,21 @@ if __name__ == '__main__':
 
         if msg is not None:
             msg = json.loads(msg)
+            insert = msg["insert"]
+            update = msg["update"]
 
-            print(msg)
+            if update:
+                print(update)
+                update_data = msg["update"]
+                condition = update_data["condition"]
+                query = update_data["query"]
+                conn_repo.update_one(condition, query)
+
+            if insert:
+                print(insert)
+                batch_list.append(insert)
+
+            print(f"time : {datetime.now()}")
             print()
-
-            batch_list.append(msg)
-            time.sleep(3)
-
-
 
 
